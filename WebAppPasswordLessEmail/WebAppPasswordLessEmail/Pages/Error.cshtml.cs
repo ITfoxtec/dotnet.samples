f30ed5d7bfea1e4f0f78fdf1e5d75debe14d4960
Anders Revsgaard <anders@itfoxtec.com>
2025-12-13 22:52:07 +0100

    clean up

--- WebAppPasswordLessEmail/WebAppPasswordLessEmail/Pages/Error.cshtml.cs
+++ WebAppPasswordLessEmail/WebAppPasswordLessEmail/Pages/Error.cshtml.cs
@@ -17,5 +17,4 @@ namespace WebAppPasswordLessEmail.Pages
             RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier;
         }
     }
-
 }
--- WebAppPasswordLessEmail/WebAppPasswordLessEmail/Pages/Privacy.cshtml.cs
+++ WebAppPasswordLessEmail/WebAppPasswordLessEmail/Pages/Privacy.cshtml.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace WebAppPasswordLessEmail.Pages
 {
@@ -9,5 +8,4 @@ namespace WebAppPasswordLessEmail.Pages
         {
         }
     }
-
 }
